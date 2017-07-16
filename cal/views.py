8258210c2f087a1c69d89f95d10878b03ddf86af
Nayoung Kim <nayoungkim@Nayoungs-MacBook-Air.local>
2017-07-16 13:14:13 +0900

    mypage custom

--- cal/views.py
+++ cal/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from .models import *
+import datetime
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
+def index(request): 
+	form = AuthenticationForm
+	return render(request,'cal/index.html',{'form':form})
+
+
+
 def test(request):
+	user = request.user
+	profile = user.profile
+	name = profile.name
+	gender = profile.gender
+
+
 	print(request.user)
 	return render(request,'cal/test.html',{})
 
-
+@login_required
 def mypage(request):
 	user = request.user
 	profile= user.profile
@@ -22,15 +37,35 @@ def mypage(request):
 
 	#최근 먹은 음식 및 운동현황
 
-	eats = Eat.objects.filter(user_id)
-	workouts = Workout.objects.all()
+	eats = Eat.objects.filter(user_id=user.pk).order_by('-date')[:10]
+	workouts = Workout.objects.filter(user_id=user.pk).order_by('-date')[:10]
+
+	today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+	today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+	eats_today = Eat.objects.filter(user_id=user.pk, date__range=(today_min, today_max))
+	workouts_today = Workout.objects.filter(user_id=user.pk, date__range=(today_min, today_max))
+	# eats_today = Eat.objects.filter(user_id=user.pk).order_by('date')[:10]
+	eat_sum = 0
+	for eat in eats_today:
+		eat_sum += eat.food.calorie * eat.amount
+
+	workout_sum = 0
+	for workout in workouts_today:
+		workout_sum += workout.exercise.calorie * workout.duration
+	
+	left_calorie = int(eat_sum - BMR - workout_sum)
 
 	context = {
 		"profile":profile,
 		"eats": eats,
 		"workouts": workouts,
-	}
+		"workouts_today": workouts_today,
+		"eats_today": eats_today,
+		"eat_sum": eat_sum,
+		"workout_sum": workout_sum,
+		"left_calorie": left_calorie,
+			}
 
-	return render(request,'cal/test.html',context)
+	return render(request,'cal/mypage.html',context)
  
 
--- calcal/urls.py
+++ calcal/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from cal import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.mypage),
+    url(r'^$', views.index),
+    url(r'^mypage/$', views.mypage),
+    url(r'^accounts/login/$',auth_views.LoginView.as_view(template_name='authentication/login.html'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
